5e776830f758ddc02943946d90301f6d60d72d12
smanager.bruno <bruno.borges@smanager.tec.br>
2024-04-23 12:25:35 -0300

    feat:adding fuctionality loading to css

--- src/app/(Auth)/template.tsx
+++ src/app/(Auth)/template.tsx
@@ -2,7 +2,7 @@ import { Logo } from '@/components/Logo'
 
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex items-center justify-between h-screen max-w-[90%] mx-auto px-24   py-4 ">
+        <div className="flex items-center justify-between h-screen max-w-[90%] mx-auto px-24   py-4  loading">
             <div>
                 <span className="flex items-center gap-2 text-light-100 text-2xl">
                     <Logo size="large" />
